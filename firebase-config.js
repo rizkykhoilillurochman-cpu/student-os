cc6f425745c459bb52e77fe531bc84d82f82735a
rizkykhoilillurochman-cpu <rizkykhoilillurochman@gmail.com>
2026-09-26 15:38:56 +0700

    Set clean Student OS Firebase configuration

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Student OS Firebase Web configuration.
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBdfWk9ZSJUUfOmrc2Bm9UqV0TYaVdKWqY",
+  authDomain: "student-os-b0d4c.firebaseapp.com",
+  projectId: "student-os-b0d4c",
+  storageBucket: "student-os-b0d4c.firebasestorage.app",
+  messagingSenderId: "204988489673",
+  appId: "1:204988489673:web:b1afc66b04afd113cad218",
+  measurementId: "G-JL5YYTPCHT",
+  appCheckSiteKey: ""
+};
